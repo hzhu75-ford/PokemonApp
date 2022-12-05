ec05a5b2bf8dfe5e374e007ce88d0aa5b070c53b
Hank Zhu <Hank_Zhu@gap.com>
2022-12-05 00:23:32 -0800

    add floating button

--- app/src/main/java/com/example/pokemon/ui/PokemonActivity.kt
+++ app/src/main/java/com/example/pokemon/ui/PokemonActivity.kt
@@ -8,7 +8,5 @@ class PokemonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 }
